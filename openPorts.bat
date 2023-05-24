2776bc3e93f7b66ebbed8d0d2d2239cebd43b102
Ezra Cooper <94017565+EzraRC@users.noreply.github.com>
2023-05-24 10:53:19 -0400

    Create openPorts.bat

---
+++ openPorts.bat
@@ -0,0 +1,18 @@
+@echo off
+
+:: IP Address variable, change if necessary.
+set "ip=192.168.1.1"
+
+:: For loop from 1 to 100 inclusive
+powershell.exe -Command "for ($port=1; $port -le 100; $port++) {
+
+	:: Tests the connection to the IP address and port.
+	if (Test-NetConnection -ComputerName '%ip%' -Port $port -InformationLevel Quiet) {
+
+		:: Print that the port is open.
+		echo Port $port is open.
+	}
+}
+
+::Pauses the script so we can read the output after it's complete.
+pause

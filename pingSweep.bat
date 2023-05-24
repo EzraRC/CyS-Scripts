7a6321d9a2a2df856378dadc199982b8c16d2771
Ezra Cooper <94017565+EzraRC@users.noreply.github.com>
2023-05-24 10:30:02 -0400

    Updated comments on pingSweep.bat

--- pingSweep.bat
+++ pingSweep.bat
@@ -1,13 +1,13 @@
-REM Starting ping sweep...
+echo Starting ping sweep...
 @echo off
-REM Removes the commands from being displayed in order to clean the output
+:: Removes the commands from being displayed in order to clean the output
 
-REM Typical for loop
+:: Typical for loop
 FOR /L %%i IN (1, 1, 254) DO (
     ping -a -n 1 192.168.1.%%i
 )
 
-REM Pauses the script so we can read the output after it's complete
+:: Pauses the script so we can read the output after it's complete
 pause
 
-REM Please note that the script assumes the subnet mask to be 255.255.255.0, which is the common default subnet mask for a typical Class C IP address range like 192.168.1.x.
+:: Please note that the script assumes the subnet mask to be 255.255.255.0, which is the common default subnet mask for a typical Class C IP address range like 192.168.1.x.
